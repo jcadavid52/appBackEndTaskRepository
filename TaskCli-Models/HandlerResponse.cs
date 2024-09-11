d69a22acd524266ec5424dd75c858f916af09fcf
jcadavid <cadavidcamilo360@gmail.com>
2024-09-11 17:13:14 -0500

    Agrego posibilidad de null en las propiedades del modelo HandlerResponse

--- TaskCli-Models/HandlerResponse.cs
+++ TaskCli-Models/HandlerResponse.cs
@@ -9,8 +9,8 @@
     }
     public class HandlerResponse
     {
-        public List<TaskModel> TasksModel;
-        public TaskModel TaskModel { get; set; }
+        public List<TaskModel>? TasksModel;
+        public TaskModel? TaskModel { get; set; }
         public ResponseResult ResponseResult { get; set; }
         public string DescriptionResult { get; set; }
 
