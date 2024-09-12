028f788334eb65c16b60f5c9a2397260e1c8d385
jcadavid <cadavidcamilo360@gmail.com>
2024-09-12 11:33:29 -0500

    Modifico funcionalidad de agregar tarea en el repositorio para archivos json

--- TaskCli-Data/RepositoryFileJson.cs
+++ TaskCli-Data/RepositoryFileJson.cs
@@ -1,12 +1,14 @@
 ﻿using Newtonsoft.Json;
+using System.Threading.Tasks;
 using TaskCli_Models;
 using TaskCli_Services;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TaskCli_Data
 {
     public class RepositoryFileJson : IRepositoryFile
     {
-        private readonly string _pathJson;
+        private readonly string _pathJson = "";
         public RepositoryFileJson()
         {
             _pathJson = Environment.GetEnvironmentVariable("PathJson");
@@ -14,7 +16,48 @@ namespace TaskCli_Data
         }
         public HandlerResponse AddTask(TaskModel taskModel)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var tasks = GetTasks();
+
+                if(tasks.TasksModel != null)
+                {
+
+                    tasks.TasksModel.Add(taskModel);
+
+                    var serializeJson = JsonConvert.SerializeObject(tasks.values, Formatting.Indented);
+
+                    File.WriteAllText(_pathJson, serializeJson);
+
+                    return new HandlerResponse
+                    {
+                        TaskModel = taskModel,
+                        ResponseResult = ResponseResult.Success,
+                        DescriptionResult = "Task registered successfully!"
+                    };
+                }
+                else
+                {
+                    return new HandlerResponse
+                    {
+                        TaskModel = null,
+                        ResponseResult = ResponseResult.TaskNotFound,
+                        DescriptionResult = "Error getting tasks"
+                    };
+                }
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                return new HandlerResponse
+                {
+                    TaskModel = null,
+                    ResponseResult = ResponseResult.UnknownError,
+                    DescriptionResult = "Error: " + ex.Message
+                };
+            }
         }
 
         public HandlerResponse GetTasks()
@@ -25,12 +68,25 @@ namespace TaskCli_Data
 
                 var tasks = JsonConvert.DeserializeObject<List<TaskModel>>(jsonString);
 
-                return (tasks, ResponseResult.Success, "");
+                return new HandlerResponse
+                {
+                    TasksModel = tasks,
+                    ResponseResult = ResponseResult.Success,
+                    DescriptionResult = string.Empty
+
+                };
             }
             catch (Exception ex)
             {
 
-                return (null, ResponseResult.UnknownError, "Error interno: " + ex.Message.ToString());
+
+                return new HandlerResponse
+                {
+                    TasksModel = null,
+                    ResponseResult = ResponseResult.UnknownError,
+                    DescriptionResult = "Error interno: " + ex.Message.ToString()
+
+                };
             }
         }
 
