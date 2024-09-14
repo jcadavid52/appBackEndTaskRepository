4ab2221849629adf00e4ae9c132df71557d550ad
jcadavid <cadavidcamilo360@gmail.com>
2024-09-14 13:01:02 -0500

    Agrego proyectos de web api a la aplicacion

--- TaskCli-Utils/obj/Debug/net8.0/TaskCli-Utils.AssemblyInfo.cs
+++ TaskCli-Utils/obj/Debug/net8.0/TaskCli-Utils.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskCli-Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fcaa366f5b48a741b7dc460ec05533915ed6b08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd94e9cf79388baa309345e12e9ae6fabfd5e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskCli-Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskCli-Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
